6cdf2cf6bfcdc2361ddebaa3ca49a04fdfc5debf
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-27 23:57:38 +0300

    added route /stats to index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,31 @@
 first part of restful api project."""
 from flask import Flask
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """returns JSON status """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    """retrieves the number of each objects by type"""
+    dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(dict)
