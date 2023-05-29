9692be0379b25bc9c15528e638a8c06f7926536f
KateMwende <kmwende419@gmail.com>
2023-05-29 19:15:37 +0000

    Edits

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,6 +12,13 @@ from models.review import Review
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.reviews import *
 
 def get(data):
     """Sends HTTP GET request"""
@@ -75,11 +82,3 @@ def put(data):
             return jsonify(found.to_dict()), 200
         else:
             abort(404)
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.reviews import *
