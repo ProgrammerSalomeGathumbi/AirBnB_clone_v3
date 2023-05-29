e83492efb52eee7caae6a500409399b3781f8643
KateMwende <kmwende419@gmail.com>
2023-05-29 22:56:21 +0000

    New user file

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 
 
 @app_views.route("/users", methods=['GET'],  strict_slashes=False)
-def get_all_states():
+def get_all_users():
     """Retrieves the list of all User objects"""
     user = storage.all(User).values()
     s_dict = []
