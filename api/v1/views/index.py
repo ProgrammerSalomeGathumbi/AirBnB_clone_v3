d34e69acb36ac0eb68b90fa53cda8b5a079e0722
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-29 12:37:45 +0300

    modified the index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,10 +22,10 @@ def api_status():
 @app_views.route("/stats", strict_slashes=False)
 def api_stats():
     """retrieves the number of each objects by type"""
-    stats_dict = {"amenities": storage.count("Amenity"),
-                  "cities": storage.count("City"),
-                  "place": storage.count("Place"),
-                  "reviews": storage.count("Review"),
-                  "states": storage.count("State"),
-                  "users": storage.count("User")}
+    stats_dict = {"amenities": storage.count(Amenity),
+                  "cities": storage.count(City),
+                  "place": storage.count(Place),
+                  "reviews": storage.count(Review),
+                  "states": storage.count(State),
+                  "users": storage.count(User)}
     return jsonify(stats_dict)
