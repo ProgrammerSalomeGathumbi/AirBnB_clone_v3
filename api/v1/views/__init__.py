7dd0be0e713375ca9f458cba91b38cf18dc08028
KateMwende <kmwende419@gmail.com>
2023-05-29 19:27:22 +0000

    Init file edit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,12 +3,6 @@
 init file
 """
 from flask import Blueprint
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.place import Place
-from models.review import Review
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
