992ab2133528af91b3db40fe1faab66b098fefaa
KateMwende <kmwende419@gmail.com>
2023-05-29 18:43:14 +0000

    Amenities and init editing

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Create a new view for Amenity objects
+"""
+from flask import Flask, request, jsonify, abort
+from api.v1.views import app_views
+from models import storage
+from models.amenities import amenities
+
+
+@app_views.route("/amenities", methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route("/amenities/<amenity_id>", methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def get_amenities(amenity_id=None):
+    """Returns GET, POST, DELETE, PUT methods"""
+    data = {
+            'str': Amenity,
+            'id': amenity_id,
+            'p_id': None,
+            'check': ['name'],
+            'ignore': ['created_at', 'updated_at', 'id']}
+    methods = {
+                'GET': get,
+                'POST': post,
+                'DELETE': delete,
+                'PUT': put}
+    if request.method in methods:
+        return methods[request.method](data)
