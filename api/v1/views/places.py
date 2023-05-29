6e5afca1d91c628e9598583eac19df4133fe90ee
KateMwende <kmwende419@gmail.com>
2023-05-29 23:30:03 +0000

    New user file

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,70 +1,81 @@
 #!/usr/bin/python3
 """
-State objects that handles all default RESTFul API actions
+Place objects that handles all default RESTFul API actions
 """
 from flask import Flask
 from flask import make_response, jsonify, abort, request
 from models import storage
-from models.state import State
+from models.place import Place
+from models.city import City
 from api.v1.views import app_views
 
 
-@app_views.route("/states", methods=['GET'],  strict_slashes=False)
-def get_all_states():
-    """Retrieves the list of all State objects"""
-    states = storage.all(State).values()
-    s_dict = []
-    for state in states:
-        s_dict.append(state.to_dict())
-    return jsonify(s_dict)
-
-
-@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
-def get_state(state_id):
-    """Retrieves a State object """
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    return jsonify(state.to_dict())
-
-
-@app_views.route("/states/<state_id>", methods=['DELETE'],
+@app_views.route("/cities/<city_id>/places", methods=['GET'],
                  strict_slashes=False)
-def delete_state(state_id):
-    """Deletes a State object """
-    state = storage.get(State, state_id)
-    if not state:
+def get_all_places(city_id):
+    """Retrieves a Place object of a city """
+    cities = storage.get(City, city_id)
+    if not cities:
         abort(404)
-    storage.delete(state)
+    c_dict = []
+    for city in places.cities:
+        c_dict.append(city.to_dict())
+    return jsonify(c_dict)
+
+
+@app_views.route("/places/<place_id>", methods=['GET'], strict_slashes=False)
+def get_place(place_id):
+    """Retrieves a place object """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    return jsonify(city.to_dict())
+
+@app_views.route("/places/<place_id>", methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place(place_id):
+    """Deletes a Place object """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    storage.delete(place)
     storage.save()
     return make_response(jsonify({}), 200)
 
 
-@app_views.route("/states", methods=['POST'],  strict_slashes=False)
-def create_state():
-    """Creates a State """
-    rjson = request.get_json()
-    if not rjson:
+@app_views.route("/cities/<city_id>/places", methods=['POST'],
+                 strict_slashes=False)
+def create_place(city_id):
+    """Creates a Place"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(400)
+    cjson = request.get_json()
+    if not cjson:
         abort(400, "Not a JSON")
-    if "name" not in rjson:
+    if "user_id" not in cjson:
+        abort(400, "Missing user_id")
+    if "name" not in cjson:
         abort(400, "Missing name")
-    state = State(** rjson)
+    cjson["city_id"] = city_id
+    place = Place(** cjson)
+    storage.new(place)
     storage.save()
-    return make_response(jsonify(state.to_dict()), 201)
+    return make_response(jsonify(place.to_dict()), 201)
 
 
-@app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """Updates a State object """
-    state = storage.get(State, state_id)
-    if not state:
+@app_views.route("/places/<place_id>", methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """Updates a Place object """
+    place = storage.get(Place, place_id)
+    if not place:
         abort(404)
     rjson = request.get_json()
     if not rjson:
         abort(400, "Not a JSON")
-    ignore = ["id", "created_at", "updated_at"]
+    ignore = ["id", "user_id", "city_id" "created_at", "updated_at"]
     for key, value in rjson.items():
         if key not in ignore:
-            setattr(state, key, value)
+            setattr(place, key, value)
     storage.save()
-    return make_responce(jsonify(state.to_dict()), 200)
+    return make_responce(jsonify(place.to_dict()), 200)
